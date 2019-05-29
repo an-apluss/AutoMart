7667b31cc16a617f42a708e994d675bb6ad27b7c
an-apluss <anuoluwapoakinseye@gmail.com>
2019-05-29 17:03:19 +0100

    feat(server) Implement user view all unsold car
    - refactor car test suites
    
    - create methods in car controller and service class to handle GET /api/v1/car?status=available functionality
    
    - refactor car dummy data
    
    - mount car controller class on GET /api/v1/car route
    
    - delivers[#166320084]

--- server/src/models/dummydata.js
+++ server/src/models/dummydata.js
@@ -17,9 +17,37 @@ const storage = {
       created_on: '2019/05/25/ 13:30:30',
       state: 'used',
       status: 'available',
-      price: 1759999.99,
-      manufacturer: 'bmw',
+      price: '1759999.99',
+      manufacturer: 'bwm',
       model: 'BMW 3 Series 320d 2014',
+      body_type: 'car',
+      imageId: 'showcase',
+      imageUrl:
+        'https://res.cloudinary.com/an-apluss/image/upload/v1558788050/AutoMart/showcase.jpg'
+    },
+    {
+      id: 2,
+      owner: 1,
+      created_on: '2019/05/25/ 13:30:30',
+      state: 'new',
+      status: 'available',
+      price: '2000000.00',
+      manufacturer: 'bmw',
+      model: 'BMW 3 Series 320d 2015',
+      body_type: 'car',
+      imageId: 'showcase',
+      imageUrl:
+        'https://res.cloudinary.com/an-apluss/image/upload/v1558788050/AutoMart/showcase.jpg'
+    },
+    {
+      id: 3,
+      owner: 1,
+      created_on: '2019/05/25/ 13:30:30',
+      state: 'new',
+      status: 'sold',
+      price: '2500000.00',
+      manufacturer: 'bmw',
+      model: 'BMW 3 Series 320d 2017',
       body_type: 'car',
       imageId: 'showcase',
       imageUrl:
