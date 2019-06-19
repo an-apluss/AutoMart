2b416c687d0ee33087780a080bc7129a63e83eb3
an-apluss <anuoluwapoakinseye@gmail.com>
2019-06-19 10:45:54 +0100

    chore(database) Refactor car endpoints to use database
    - create file to generate default car advert in the database
    
    - refactor package.json file with script to generate default car advert
    
    - update .eslint.json file to turn off no-param-reassign rule
    
    - update car model class with the method to post a car advert in the database
    
    - refactor the method to post a car advert in the car service class to use database
    
    - update car model class with method to find a car by ID in the database
    
    - update car model class with the method to update posted car advert status in the database
    
    - refactor the method to update car status in the car service class to use database
    
    - refactor the method to update car price in the car service class to use database
    
    - refactor the method to fetch a specific car advert in the car service class to use database
    
    - update car model class with the method to fetch unsold cars in the database
    
    - refactor the method to fetch all unsold car in the car service class to use database
    
    - update car model class with the method to fetch unsold cars within price range in the database
    
    - refactor the method to fetch unsold car within price range in the car service class to use database
    
    - update car model class with the method to delete a specific car advert in the database
    
    - refactor the method to delete a specific car advert in the car service class to use database
    
    - update car model class with the method to fetch all car advert both sold or unsold in the database
    
    - refactor the method to fetch all car both sold or unsold in the car service class to use database
    
    - refactor the method to fetch car details by ID in the car service class to use database
    
    - refactor the method to fetch car base on status and state in the car service class to use database
    
    - update car model class with the method to fetch car base on status and state in the database
    
    - finishes[#166784074]

--- server/src/models/carModel.js
+++ server/src/models/carModel.js
@@ -1,3 +1,5 @@
+import database from '../database/index';
+
 /**
  *
  *
@@ -6,31 +8,126 @@
  */
 export default class Car {
   /**
-   *Creates an instance of Car.
-   * @param {Integer} id
-   * @param {Integer} owner
-   * @param {String} state
-   * @param {Date} [created_on = new Date()]
-   * @param {String} [status = 'available']
-   * @param {Float} price
-   * @param {String} manufacturer
-   * @param {String} model
-   * @param {String} bodyType
-   * @param {String} imageId
-   * @param {String} imageUrl
+   *
+   * Create car post advert in the database
+   * @static
+   * @param {Object} carInfo
+   * @returns {Object}
    * @memberof Car
    */
-  constructor(id, owner, state, price, manufacturer, model, bodyType, imageId, imageUrl) {
-    this.id = id;
-    this.owner = owner;
-    this.created_on = new Date();
-    this.state = state;
-    this.status = 'available';
-    this.price = price;
-    this.manufacturer = manufacturer;
-    this.model = model;
-    this.body_type = bodyType;
-    this.imageId = imageId;
-    this.imageUrl = imageUrl;
+  static async create(carInfo) {
+    const { owner, state, price, manufacturer, model, bodyType, imageId, imageUrl } = carInfo;
+
+    const sqlQuery = `INSERT INTO cars (owner, state, price, manufacturer, model, body_type, imageId, imageUrl) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) returning *`;
+    const values = [owner, state, price, manufacturer, model, bodyType, imageId, imageUrl];
+
+    const { rows } = await database.query(sqlQuery, values);
+
+    return rows[0];
+  }
+
+  /**
+   *
+   * Fetch posted car advert details by ID from the database
+   * @static
+   * @param {Integer} carId
+   * @returns {Object|Boolean} return object if ID is found or boolean if ID can't be found
+   * @memberof Car
+   */
+  static async findById(carId) {
+    const sqlQuery = `SELECT * FROM cars where id = $1`;
+    const { rows, rowCount } = await database.query(sqlQuery, [carId]);
+
+    if (rowCount > 0) return rows[0];
+
+    return false;
+  }
+
+  /**
+   *
+   * Update field with new value in the database table
+   * @static
+   * @param {Integer} carId car id of the posted advert to be updated
+   * @param {String} field table field to be updated
+   * @param {Integer|String} value new value
+   * @returns {Object} updated field
+   * @memberof Car
+   */
+  static async update(carId, field, value) {
+    const sqlQuery = `update cars set ${field} = $1 where id = $2 returning ${field}`;
+    const { rows } = await database.query(sqlQuery, [value, carId]);
+    return rows[0];
+  }
+
+  /**
+   *
+   * Fetch car advert which status reads available in the database
+   * @static
+   * @param {string} status
+   * @returns {Array} List of unsold cars in the database
+   * @memberof Car
+   */
+  static async findByStatus(status) {
+    const sqlQuery = `SELECT * FROM cars where status = $1`;
+    const { rows } = await database.query(sqlQuery, [status]);
+    return rows;
+  }
+
+  /**
+   *
+   * Fetch all unsold car advert within specific price range in the database
+   * @static
+   * @param {String} status status of the car advert
+   * @param {Float} min_price minimum price of car advert to be fetch
+   * @param {Float} max_price maximum price of car advert to be fetch
+   * @returns {Array} list of unsold car advert within price range
+   * @memberof Car
+   */
+  static async findByPriceRange(status, min_price, max_price) {
+    const sqlQuery = `SELECT * FROM cars WHERE status = $1 AND price >= $2 AND price <= $3`;
+    const values = [status, min_price, max_price];
+    const { rows } = await database.query(sqlQuery, values);
+    return rows;
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {Integer} carId car id of the car advert to be removed from the database
+   * @memberof Car
+   */
+  static async remove(carId) {
+    const sqlQuery = `DELETE FROM cars WHERE id = $1`;
+    await database.query(sqlQuery, [carId]);
+  }
+
+  /**
+   *
+   * Fetch all car advert whether sold or unsold in the database
+   * @static
+   * @returns {Object} List of all car whether unsold or sold
+   * @memberof Car
+   */
+  static async findAll() {
+    const sqlQuery = `SELECT * FROM cars`;
+    const { rows } = await database.query(sqlQuery, []);
+    return rows;
+  }
+
+  /**
+   *
+   * Fetch unsold car advert which status reads either new or used
+   * @static
+   * @param {String} status the status of the car advert i.e available
+   * @param {String} state the state of the car advert i.e used or new
+   * @returns {Array}
+   * @memberof Car
+   */
+  static async findByStatusAndState(status, state) {
+    const sqlQuery = `SELECT * FROM cars WHERE status = $1 AND state = $2`;
+    const values = [status, state];
+    const { rows } = await database.query(sqlQuery, values);
+    return rows;
   }
 }
